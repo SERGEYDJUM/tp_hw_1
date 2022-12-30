be0524eee72d2c922fd8123e1934e65c8eb10470
Sergey Dzhumageldiev <sergey_dzhumageldiev@outlook.com>
2022-12-30 19:56:19 +0300

    Rework measuring and tests

--- benchmark.cpp
+++ benchmark.cpp
@@ -5,47 +5,59 @@
 #include <iostream>
 #include "solutions.cpp" 
 
-Solution PROBLEM{};
-std::ostringstream TEST_DATA{};
+struct TestData {
+    std::mt19937 mte{1337};
+    std::uniform_int_distribution<> distr{0, 200000};
 
-int measure(bool second, int n) {
-    PROBLEM.prepare(TEST_DATA.str());
-    auto start = std::chrono::system_clock::now();
-    if (second) {
-        PROBLEM.solve_second(n);
-    } else {
-        PROBLEM.solve_first(n);
+    std::vector<int> get_case(int size) {
+        std::vector<int> test_case{};
+        for (size_t j = 0; j < size; j++) {
+            test_case.push_back(distr(mte));
+        }
+        return test_case;
     }
-    auto end = std::chrono::system_clock::now();
-    return std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-}
+};
 
-int main() {
-    std::mt19937 rng{1337};
-    unsigned int test_values[20000];
-    size_t j = 0;
-    while (j < 20000) {
-        auto num = rng();
-        if (num < 200001) {
-            test_values[j] = num;
-            ++j;
+struct Benchmark {
+    FirstSolution fsol{};
+    SecondSolution ssol{};
+    TestData tests{};
+
+    std::vector<int> sizes{};
+    std::vector<int> res_first{};
+    std::vector<int> res_second{};
+
+    Benchmark(int step = 10000) {
+        for (size_t i = 0; i <= 200000; i+=step) {
+            sizes.push_back(i);
         }
     }
 
-    std::vector<int> results[2];
-    for (size_t i = 0; i < 20000; i++) {
-        TEST_DATA << test_values[i] << ' ';
-        results[0].push_back(measure(false, i+1));
-        // results[1].push_back(measure(true, i+1));
-    }
-    
-
-    std::vector<int> range;
-    for (size_t i = 1; i < 20001; i++) {
-        range.push_back(i);
+    int measure(std::vector<int> &test_case, ISolution &solver) {
+        auto start = std::chrono::high_resolution_clock::now();
+        solver(test_case);
+        auto end = std::chrono::high_resolution_clock::now();
+        return std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
     }
 
-    matplot::plot(range, results[0], ".");
-    // matplot::plot(range, results[1], "+");
+    void run(int avg_over = 5) {
+        for (size_t j = 0; j < sizes.size(); j++) {
+            int time_sum_f = 0;
+            int time_sum_s = 0;
+            for (size_t i = 0; i < avg_over; i++) {
+                auto test_case = tests.get_case(sizes[j]);
+                time_sum_f += measure(test_case, fsol);
+                time_sum_s += measure(test_case, ssol);
+            }
+            res_first.push_back(time_sum_f/avg_over);
+            res_second.push_back(time_sum_s/avg_over);
+        }
+    }
+};
+
+int main() {
+    Benchmark bench{5000};
+    bench.run(5);
+    matplot::plot(bench.sizes, bench.res_first);
     matplot::show();
 }
--- solutions.cpp
+++ solutions.cpp
@@ -3,43 +3,35 @@
 #include <vector>
 #include <unordered_set>
 #include <sstream>
-#include <string>
 
-struct Solution {
-    std::istringstream cin;
-    std::ostringstream cout;
+struct ISolution {
+    virtual void operator()(std::vector<int> &cin) const = 0;
+};
 
-    Solution() {}
-
-    void prepare(const std::string &inp) {
-        cin = std::istringstream(inp);
-    }
-
-    void solve_first(int n) {
+struct FirstSolution : ISolution {
+    void operator()(std::vector<int> &cin) const override {
+        int n = cin.size();
         std::vector<int> as(200001, -1);
         for (int i = 0; i < n; i += 1) {
-            int idx = 0;
-            cin >> idx;
+            int idx = cin[i];
             as[idx] = n - i - 1;
         }
-        cout << std::distance(as.begin(), std::max_element(as.begin(), as.end()));
+        std::distance(as.begin(), std::max_element(as.begin(), as.end()));
     }
+};
 
-    void solve_second(int n) {
-        std::vector<int> data(n, 0);
-        for (int i = 0; i < n; i += 1) {
-            cin >> data[i];
-        }
+struct SecondSolution : ISolution {
+    void operator()(std::vector<int> &cin) const override {
+        int n = cin.size();
         std::unordered_set<int> unique;
         unique.reserve(200000);
         int idx_unique = n;
         for (int i = n - 1; 0 <= i; i -= 1) {
-            if (!unique.contains(data[i]))
+            if (!unique.contains(cin[i]))
             {
-                idx_unique = data[i];
+                idx_unique = cin[i];
                 unique.insert(idx_unique);
             }
         }
-        cout << idx_unique;
     }
 };
